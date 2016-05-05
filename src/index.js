315ab237b93545548a3fcae8e44085bd0bb48dc9
Dafrok <o.o@mug.dog>
2016-05-05 14:04:59 +0800

    change data format & add callbacks

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 $.fn.giftCombo = function (option) {
     option = option || {}
     var classActive = option.classActive || 'gift-combo-active'
-    // var oneGift =  option.oneGift || new Function
-    // var oneCombo = option.oneCombo || new Function
-    // var allCombo = option.allCombo || new Function
+    var oneGift =  option.oneGift || new Function
+    var oneCombo = option.oneCombo || new Function
+    var allCombo = option.allCombo || new Function
     var $this = $(this)
     var combo = 0
     var that = this
@@ -12,25 +12,28 @@ $.fn.giftCombo = function (option) {
 
     $this.on('animationend', function (e) {
         if ($this.hasClass(classActive)) {
-            that.giftQueue[0].count--
+            that.giftQueue[0]--
             $this.removeClass(classActive)
             $this[0].clientWidth
+            oneGift()
             gifting()
         }
     })
     function gifting () {
         if (that.giftQueue.length) {
-            if (that.giftQueue[0].count) {
+            if (that.giftQueue[0]) {
                 combo++
                 $this.text(combo)
                 $this.addClass(classActive)
             } else {
                 combo = 0
                 that.giftQueue.shift()
+                oneCombo()
                 gifting()
             }
         } else {
           	$this.text('')
+            allCombo()
             that.isGifting = false
         }
     }
@@ -46,23 +49,20 @@ $.fn.giftCombo = function (option) {
 }
 
 var $giftTrigger = $('#gift-trigger')
+
 var $giftCombo = $('#gift-combo').giftCombo({
-    // oneGift: function (data) {
-    //     console.log(data)
-    // },
-    // oneCombo: function (data) {
-    //     console.log(data)
-    // },
-    // allCombo: function (data) {
-    //     console.log(data)
-    // }
-})
-$giftTrigger.on('click', function () {
-    var data = {
-        count: 0 | Math.random() * 3 + 3,
-        // name: ['张三', '李四', '王五', '赵六'][0 | Math.random() * 4]
+    oneGift: function () {
+        console.log('One gift!')
+    },
+    oneCombo: function () {
+        console.log('One combo!')
+    },
+    allCombo: function () {
+        console.log('All combo!')
     }
-    $giftCombo.send({
-        count: data.count
-    })
+})
+
+$giftTrigger.on('click', function () {
+    var count = 0 | Math.random() * 3 + 3
+    $giftCombo.send(count)
 })
